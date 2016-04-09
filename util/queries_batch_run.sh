7fe553584811695735b63f5eb27d194b32711f7d
Derek Roberts <derek.roberts@gmail.com>
2016-04-08 22:54:44 -0700

    Removed more old PDC files

--- util/queries_batch_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# Kick off run_batch_queries.sh in composer
-#
-sudo docker exec composer /app/util/run_batch_queries.sh
--- util/queries_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-set -e -o nounset
-
-sudo docker exec hapi /app/queryImporter/import_queries.sh
